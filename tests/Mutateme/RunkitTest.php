5ea59b70fae6adb1886609ed70fe98081d66b9c2
Padraic Brady <padraic@padraic-desktop.(none)>
2009-02-24 21:26:19 +0000

    Added static method mutation support (if available)

--- tests/Mutateme/RunkitTest.php
+++ tests/Mutateme/RunkitTest.php
@@ -16,6 +16,8 @@ class Mutateme_RunkitTest extends PHPUnit_Framework_TestCase
         $this->root = dirname(__FILE__) . '/_files';
     }
 
+    // public methods
+
     public function testShouldApplyGivenMutationsUsingRunkitToReplaceEffectedMethods()
     {
         $mutation = array(
@@ -50,6 +52,42 @@ class Mutateme_RunkitTest extends PHPUnit_Framework_TestCase
         $math = new RunkitTest_Math1;
         $runkit->reverseMutation($mutation);
         $this->assertEquals(2, $math->add(1,1));
+    }
+
+    // public static methods
+
+    public function testShouldApplyGivenMutationsUsingRunkitToReplaceEffectedStaticMethods()
+    {
+        $mutation = array(
+            'file' => $this->root . '/runkit/Math2.php',
+            'class' => 'RunkitTest_Math2',
+            'method' => 'add',
+            'args' => '$op1,$op2',
+            'tokens' => array(array(335,'return',7), array(309,'$op1',7), '+', array(309,'$op2',7), ';'),
+            'index' => 2,
+            'mutation' => new Mutateme_Mutation_OperatorAddition($this->root . '/runkit/Math2.php')
+        );
+        $runkit = new Mutateme_Runkit;
+        $runkit->applyMutation($mutation);
+        $this->assertEquals(0, RunkitTest_Math2::add(1,1));
+        $runkit->reverseMutation($mutation);
+    }
+
+    public function testShouldRevertToOriginalStaticMethodBodyWhenRequested()
+    {
+        $mutation = array(
+            'file' => $this->root . '/runkit/Math2.php',
+            'class' => 'RunkitTest_Math2',
+            'method' => 'add',
+            'args' => '$op1,$op2',
+            'tokens' => array(array(335,'return',7), array(309,'$op1',7), '+', array(309,'$op2',7), ';'),
+            'index' => 2,
+            'mutation' => new Mutateme_Mutation_OperatorAddition($this->root . '/runkit/Math2.php')
+        );
+        $runkit = new Mutateme_Runkit;
+        $runkit->applyMutation($mutation);
+        $runkit->reverseMutation($mutation);
+        $this->assertEquals(2, RunkitTest_Math2::add(1,1));
     }
 }
 
