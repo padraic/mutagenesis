b4437c3b8624c6dcb37613dcf69d353388fd3055
Padraic Brady <padraic.brady@yahoo.com>
2010-05-07 19:14:20 +0100

    Added runkit class - tested against 3rd-Party runkit extension (works with PHP 5.3 flawlessly)

---
+++ library/Mutateme/Runkit.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Mutateme
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://github.com/padraic/mutateme/blob/rewrite/LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to padraic@php.net so we can send you a copy immediately.
+ *
+ * @category   Mutateme
+ * @package    Mutateme
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2010 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    http://github.com/padraic/mutateme/blob/rewrite/LICENSE New BSD License
+ */
+
+namespace Mutateme;
+
+class Runkit
+{
+
+    /**
+     * Method signature hash appended to a replaced method's name so it can
+     * be reinstated later without any need to separately store entire method
+     * related code blocks.
+     *
+     * @var string
+     */
+    protected $_methodPreserveCode = '';
+
+    /**
+     * Apply a mutation to the relevant file
+     *
+     * @param array $mutation
+     */
+    public function applyMutation(array $mutation)
+    {
+        require_once $mutation['mutation']->getFilename();
+        $newBlock = $mutation['mutation']
+            ->mutate($mutation['tokens'], $mutation['index']);
+        $this->_methodPreserveCode = md5($mutation['method']);
+        if (runkit_method_rename(
+            $mutation['class'],
+            $mutation['method'],
+            $mutation['method'] . $this->_methodPreserveCode
+        ) == false) {
+            throw new \RuntimeException(
+                'runkit_method_rename() failed from ' . $mutation['class']
+                . '::' . $mutation['method'] . ' to ' . $mutation['class']
+                . '::' . $mutation['method'] . $this->_methodPreserveCode
+                . ' (mutation application)'
+            );
+        }
+        if(runkit_method_add(
+            $mutation['class'],
+            $mutation['method'],
+            $mutation['args'],
+            $newBlock,
+            $this->getMethodFlags($mutation)
+        ) == false) {
+            throw new \RuntimeException(
+                'runkit_method_add() failed when adding original '
+                . $mutation['class'] . '::' . $method['method']
+                . '(' . var_export($mutation['args']) . ') with ' . $newBlock
+            );
+        }
+    }
+
+    /**
+     * Reverse a previously applied mutation to the given file
+     *
+     * @param array $mutation
+     */
+    public function reverseMutation(array $mutation)
+    {
+        if(runkit_method_remove(
+            $mutation['class'],
+            $mutation['method']
+        ) == false) {
+            throw new \RuntimeException(
+                'runkit_method_remove() failed attempting to remove '
+                . $mutation['class'] . '::' . $mutation['method']
+            );
+        }
+        if(runkit_method_rename(
+            $mutation['class'],
+            $mutation['method'] . $this->_methodPreserveCode,
+            $mutation['method']
+        ) == false) {
+            throw new \RuntimeException(
+                'runkit_method_rename() failed renaming from '
+                . $mutation['class'] . '::' . $mutation['method']
+                . $this->_methodPreserveCode . ' to ' . $mutation['class']
+                . '::' . $mutation['method'] . ' (mutation reversal)'
+            );
+        }
+    }
+
+    /**
+     * Get the appropriate ext/runkit method flag value to use during
+     * a replacement via the runkit methods
+     *
+     * @param array $mutation
+     * @return int
+     */
+    public function getMethodFlags(array $mutation)
+    {
+        $reflectionClass = new \ReflectionClass($mutation['class']);
+        $reflectionMethod = $reflectionClass->getMethod(
+            $mutation['method'] . $this->_methodPreserveCode
+        );
+        $static = null;
+        $access = null;
+        if ($reflectionMethod->isPublic()) {
+            $access = RUNKIT_ACC_PUBLIC;
+        } elseif ($reflectionMethod->isProtected()) {
+            $access = RUNKIT_ACC_PROTECTED;
+        } elseif ($reflectionMethod->isPrivate()) {
+            $access = RUNKIT_ACC_PRIVATE;
+        }
+        if (defined('RUNKIT_ACC_STATIC') && $reflectionMethod->isStatic()) {
+            $static = RUNKIT_ACC_STATIC;
+        }
+        if (!is_null($static)) {
+            return $access | $static;
+        }
+        return $access;
+    }
+    
+}
---
+++ tests/Mutateme/RunkitTest.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Mutateme
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://github.com/padraic/mutateme/blob/rewrite/LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to padraic@php.net so we can send you a copy immediately.
+ *
+ * @category   Mutateme
+ * @package    Mutateme
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2010 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    http://github.com/padraic/mutateme/blob/rewrite/LICENSE New BSD License
+ */
+
+require_once 'Mutateme/Mutation/MutationAbstract.php';
+
+require_once 'Mutateme/Mutation/OperatorAddition.php';
+
+require_once 'Mutateme/Runkit.php';
+
+class Mutateme_RunkitTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        $this->root = dirname(__FILE__) . '/_files';
+    }
+    
+    public function testShouldApplyGivenMutationsUsingRunkitToReplaceEffectedMethods()
+    {
+        $mutation = array(
+            'file' => $this->root . '/runkit/Math1.php',
+            'class' => 'RunkitTest_Math1',
+            'method' => 'add',
+            'args' => '$op1,$op2',
+            'tokens' => array(array(335,'return',7), array(309,'$op1',7), '+', array(309,'$op2',7), ';'),
+            'index' => 2,
+            'mutation' => new \Mutateme\Mutation\OperatorAddition($this->root . '/runkit/Math1.php')
+        );
+        $runkit = new \Mutateme\Runkit;
+        $runkit->applyMutation($mutation);
+        $math = new RunkitTest_Math1;
+        $this->assertEquals(0, $math->add(1,1));
+        $runkit->reverseMutation($mutation);
+    }
+
+    public function testShouldRevertToOriginalMethodBodyWhenRequested()
+    {
+        $mutation = array(
+            'file' => $this->root . '/runkit/Math1.php',
+            'class' => 'RunkitTest_Math1',
+            'method' => 'add',
+            'args' => '$op1,$op2',
+            'tokens' => array(array(335,'return',7), array(309,'$op1',7), '+', array(309,'$op2',7), ';'),
+            'index' => 2,
+            'mutation' => new \Mutateme\Mutation\OperatorAddition($this->root . '/runkit/Math1.php')
+        );
+        $runkit = new \Mutateme\Runkit;
+        $runkit->applyMutation($mutation);
+        $math = new RunkitTest_Math1;
+        $runkit->reverseMutation($mutation);
+        $this->assertEquals(2, $math->add(1,1));
+    }
+
+    public function testShouldApplyGivenMutationsUsingRunkitToReplaceEffectedStaticMethods()
+    {
+        $mutation = array(
+            'file' => $this->root . '/runkit/Math2.php',
+            'class' => 'RunkitTest_Math2',
+            'method' => 'add',
+            'args' => '$op1,$op2',
+            'tokens' => array(array(335,'return',7), array(309,'$op1',7), '+', array(309,'$op2',7), ';'),
+            'index' => 2,
+            'mutation' => new \Mutateme\Mutation\OperatorAddition($this->root . '/runkit/Math2.php')
+        );
+        $runkit = new \Mutateme\Runkit;
+        $runkit->applyMutation($mutation);
+        $this->assertEquals(0, RunkitTest_Math2::add(1,1));
+        $runkit->reverseMutation($mutation);
+    }
+
+    public function testShouldRevertToOriginalStaticMethodBodyWhenRequested()
+    {
+        $mutation = array(
+            'file' => $this->root . '/runkit/Math2.php',
+            'class' => 'RunkitTest_Math2',
+            'method' => 'add',
+            'args' => '$op1,$op2',
+            'tokens' => array(array(335,'return',7), array(309,'$op1',7), '+', array(309,'$op2',7), ';'),
+            'index' => 2,
+            'mutation' => new \Mutateme\Mutation\OperatorAddition($this->root . '/runkit/Math2.php')
+        );
+        $runkit = new \Mutateme\Runkit;
+        $runkit->applyMutation($mutation);
+        $runkit->reverseMutation($mutation);
+        $this->assertEquals(2, RunkitTest_Math2::add(1,1));
+    }
+}
+
+class StubMutatemeMutation1 extends \Mutateme\Mutation\MutationAbstract
+{
+    public function getMutation(array $tokens, $index){}
+}
